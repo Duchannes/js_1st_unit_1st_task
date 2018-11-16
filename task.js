6d107a4e26c6540df3d2714290b4e9ec02c6d663
Anton_viarenich <Anton_viarenich@epam.com>
2018-11-16 20:40:09 +0300

    refactored

--- task.js
+++ task.js
@@ -1,50 +1,28 @@
-let fs = require('fs')
+const fs = require('fs')
+const readline = require('readline-sync')
 
-let readline = require('readline-sync')
-let path = readline.question('Input file name | file absolute path:\n')
-let encoding = getEncodingAndCheck()
+const path = readline.question('Input file name | file absolute path:\n')
+const encoding = readline.question('Input file encoding:\n')
 
-function getEncodingAndCheck () {
-  let encodings = ['ascii', 'base64', 'binary', 'hex', 'usc2', 'usc-2', 'utf16le', 'tf-16le', 'utf8', 'utf-8', 'latin1']
-  let encoding
-  do {
-    encoding = readline.question('Input file encoding:\n' + encodings + '\n')
-  }
-  while (encodings.findIndex((element) => {
-    return element === encoding
-  }) === -1)
-  return encoding
-}
-
-let promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    let text
+const getFileContent = (path, encoding) => {
+  return new Promise((resolve, reject) => {
     fs.readFile(path, encoding, function (error, content) {
       if (error) {
         reject(error)
       } else {
-        text = content.split('\r\n')
+        const text = content.split('\r\n')
         resolve(text)
       }
     })
-  }, 100)
-}).then(result => {
-  let newResult = []
-  if (result.length > 1) {
-    console.log('---------\nEven lines')
-    for (let i = 1; i < result.length; i += 2) {
-      newResult.push(result[i])
-      console.log(result[i])
-    }
-  } else {
-    throw new Error('There are no lines matching the condition')
-  }
-  return newResult
-}).then(result => {
-  fs.writeFile('log.txt', result.join('\r\n'), encoding, () => { console.log('-----\nFile log.txt was created') })
-  return result
-}).catch(error => {
-  console.log(error.toString())
-})
+  })
+}
 
-promise.then()
+getFileContent(path, encoding)
+  .then(result => {
+    const evenLines = result.filter((element, index) => { if (index % 2 === 1) return element })
+    if (evenLines.length) {
+      console.log(evenLines.join('\n'))
+    } else {
+      throw new Error('There are no lines matching the condition')
+    }
+  }).catch(error => console.log(error.message))
